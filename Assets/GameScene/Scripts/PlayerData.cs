9e493daaec85100a0bfff4eb02065259de74b49a
Younkyu <younkyu2004@gmail.com>
2022-04-28 22:37:48 -0700

    Fixed bug where players with same number shares UI Numbers

--- Assets/GameScene/Scripts/PlayerData.cs
+++ Assets/GameScene/Scripts/PlayerData.cs
@@ -38,6 +38,26 @@ public class PlayerData : MonoBehaviour
 
     void Update()
     {
+        if (player1 == null)
+            player1 = bgm.getGroupWorld(bgm.getPlayerGroup()).transform.Find("Canvas").Find("PlayerPanels").Find("player1").gameObject;
+        if (player2 == null)
+            player2 = bgm.getGroupWorld(bgm.getPlayerGroup()).transform.Find("Canvas").Find("PlayerPanels").Find("player2").gameObject;
+        if (player3 == null)
+            player3 = bgm.getGroupWorld(bgm.getPlayerGroup()).transform.Find("Canvas").Find("PlayerPanels").Find("player3").gameObject;
+        if (player4 == null)
+            player4 = bgm.getGroupWorld(bgm.getPlayerGroup()).transform.Find("Canvas").Find("PlayerPanels").Find("player4").gameObject;
+
+        if (playerNumber == 0)
+        {
+            for (int i = 1; i <= pGroup.m_playerGroups[bgm.getPlayerGroup() - 1].Count; i++)
+            {
+                if (pGroup.m_playerGroups[bgm.getPlayerGroup() - 1][i - 1] == GameLiftManager.GetInstance().m_PeerId)
+                {
+                    playerNumber = i;
+                }
+            }
+        }
+
         if (player1 != null)
             player1.transform.Find("playerPoints").GetComponent<Text>().text = "Stars: " + p1Stars + "\nMove Pts: " + p1MovePoints;
         if (player2 != null)
@@ -50,55 +70,42 @@ public class PlayerData : MonoBehaviour
 
     public void readData(string _id, float[] _f)
     {
-        // UPDATE THE APPROPRIATE PLAYERPANEL VARIABLES
-        switch (_f[0])
+        if (_f[0] == bgm.getPlayerGroup())
         {
-            case 1:
-                p1Stars = (int)_f[1];
-                p1MovePoints = (int)_f[2];
-                break;
+            switch (_f[1])
+            {
+                case 1:
+                    p1Stars = (int)_f[2];
+                    p1MovePoints = (int)_f[3];
+                    break;
 
-            case 2:
-                p2Stars = (int)_f[1];
-                p2MovePoints = (int)_f[2];
-                break;
+                case 2:
+                    p2Stars = (int)_f[2];
+                    p2MovePoints = (int)_f[3];
+                    break;
 
-            case 3:
-                p3Stars = (int)_f[1];
-                p3MovePoints = (int)_f[2];
-                break;
+                case 3:
+                    p3Stars = (int)_f[2];
+                    p3MovePoints = (int)_f[3];
+                    break;
 
-            case 4:
-                p4Stars = (int)_f[1];
-                p4MovePoints = (int)_f[2];
-                break;
+                case 4:
+                    p4Stars = (int)_f[2];
+                    p4MovePoints = (int)_f[3];
+                    break;
+            }
         }
     }
 
     public void sendData()
     {
-        if (player1 == null)
-            player1 = bgm.getGroupWorld(bgm.getPlayerGroup()).transform.Find("Canvas").Find("PlayerPanels").Find("player1").gameObject;
-        if (player2 == null)
-            player2 = bgm.getGroupWorld(bgm.getPlayerGroup()).transform.Find("Canvas").Find("PlayerPanels").Find("player2").gameObject;
-        if (player3 == null)
-            player3 = bgm.getGroupWorld(bgm.getPlayerGroup()).transform.Find("Canvas").Find("PlayerPanels").Find("player3").gameObject;
-        if (player4 == null)
-            player4 = bgm.getGroupWorld(bgm.getPlayerGroup()).transform.Find("Canvas").Find("PlayerPanels").Find("player4").gameObject;
-
-        for (int i = 1; i <= pGroup.m_playerGroups[bgm.getPlayerGroup() - 1].Count; i++)
-        {
-            if (pGroup.m_playerGroups[bgm.getPlayerGroup() - 1][i - 1] == GameLiftManager.GetInstance().m_PeerId)
-            {
-                playerNumber = i;
-            }
-        }
         gameObject.GetComponent<ASLObject>().SendAndSetClaim(() =>
         {
-            float[] sendValue = new float[3];
-            sendValue[0] = playerNumber;
-            sendValue[1] = DiceRoll.starCount;
-            sendValue[2] = DiceRoll.movePoints;
+            float[] sendValue = new float[4];
+            sendValue[0] = bgm.getPlayerGroup();
+            sendValue[1] = playerNumber;
+            sendValue[2] = DiceRoll.starCount;
+            sendValue[3] = DiceRoll.movePoints;
 
             gameObject.GetComponent<ASLObject>().SendFloatArray(sendValue);
         });
